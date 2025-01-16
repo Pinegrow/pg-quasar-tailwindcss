46cbe24f2e446acd82c960405b31ee7946f2c457
Akayy <techakayy@gmail.com>
2025-01-16 12:27:06 +1100

    Added pinegrow.config.cjs

---
+++ pinegrow.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  liveDesigner: {
+    // TODO: This file is not required from Vue Designer update v1.61
+    configPath: './quasar.config.ts',
+    //... existing config
+  },
+}
